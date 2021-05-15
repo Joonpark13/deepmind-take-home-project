02f0b09c77302f50b3288e4b69785982a09d0630
Joon Park <joonhpark@google.com>
2021-05-15 17:50:04 -0500

    Refactor TaskScoreCard to use makeStyles hook.

--- src/TaskScoreCard.js
+++ src/TaskScoreCard.js
@@ -1,15 +1,23 @@
 import { Card, CardContent, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import 'css-percentage-circle/css/circle.css';
-import './TaskScoreCard.css';
+
+const useStyles = makeStyles({
+  taskScore: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  },
+});
 
 export default function TaskScoreCard({ categoryName, score }) {
+  const styles = useStyles();
+
   return (
     <Card>
       <CardContent>
         <Typography variant="h6" gutterBottom>{categoryName}</Typography>
 
         <div className={`c100 p${Math.round(score)} green`}>
-          <span className="task-score">{Math.round(score)}%</span>
+          <span className={styles.taskScore}>{Math.round(score)}%</span>
           <div className="slice">
               <div className="bar"></div>
               <div className="fill"></div>
